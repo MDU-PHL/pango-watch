9b8d5ddd0b19623ab02df865741ea138bf3715e1
wytamma <wytamma.wirth@me.com>
2022-08-24 21:59:56 +1000

    :sparkles: add changes to README

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # pango-watch 
 
+[![.github/workflows/run.yml](https://github.com/MDU-PHL/pango-watch/actions/workflows/run.yml/badge.svg)](https://github.com/MDU-PHL/pango-watch/actions/workflows/run.yml)
+
 Watch for pango updates!
+
 
--- watch.py
+++ watch.py
@@ -1,22 +1,12 @@
-# curl https://api.github.com/repos/cov-lineages/pango-designation/contents/lineage_notes.txt
-
 from dataclasses import dataclass
 from datetime import datetime
 from difflib import Differ
-from fileinput import filelineno
 import json
-from multiprocessing.spawn import get_preparation_data
 from pathlib import Path
 import requests
 import typer
 import base64
 
-
-
-def download_text(download_url):
-    r = requests.get(download_url)
-    file = r.text
-    return file
 
 @dataclass
 class File:
@@ -94,10 +84,31 @@ def main(url: str = typer.Argument("https://api.github.com/repos/cov-lineages/pa
 
     last_text = last_file.text
     diff = new_file.diff(last_text)
-    changes = db.get('changes')
+    changes: list = db.get('changes')
     changes.append({'sha':new_file.sha, 'datetime':str(datetime.now()), 'changes': diff})
     db.put('changes', changes)
     db.put('last', new_file.data)
+
+    readme = []
+    with open('README.md') as f:
+        lines = f.readlines()
+        for line in lines:
+            if line.startswith("## Changes"):
+                print('## Changes')
+                break
+            readme.append(line)
+
+    # update README
+    with open('README.md', 'w') as f:
+        f.writelines(readme)
+        f.write("## Changes\n")
+        for change in changes[::-1]:
+            f.write(f"### {change['datetime']}\n")
+            f.write(f"*{change['sha']}*\n")
+            for c in change['changes']:
+                f.write(f"- \{c}\n")
+
+    # send slack 
 
 if __name__ == "__main__":
     typer.run(main)
